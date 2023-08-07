aa89d6bd1b76054c01ef24fbac582e0ebfa344c1
summekoushal <2020bcs003@sggs.ac.in>
2023-08-08 01:52:01 +0530

    added profile section styles

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,38 +2,24 @@ import React from 'react'
 
 export default function Profile() {
 
-  // var typed = new Typed(".auto-type", {
-  //   strings: ["sdfdsf", "ertwet"],
-  //   typeSpeed: 50,
-  // });
-
   return (
-    <div className="profile-container ">
+    <div className="profile-container">
       <div className="profile-text text-primary">
-        <span className="profile-hello gradient-color">Hello There,</span>{" "}
-        <br />
-        <span
-          style={{
-            marginTop: "5px",
-          }}
-        >
-          <span>
-            My name is Summer Koushal <br />
+        <span className="profile-hello gradient-color">Hello There !!</span>
+        <br/>
+        <span style={{ marginTop: "5px", }}>
+          My name is Summer <br />
+        </span>
+        <span style={{ marginTop: "5px", }} >
+          I am a <span>{" "}</span>
+          <span className="auto-type">
+             Developer : )
           </span>
         </span>
-        <span
-          style={{
-            marginTop: "5px",
-          }}
-        >
-          I am a <span className="auto-type">Developer...</span>
-        </span>
       </div>
+
       <div className="profile-photo">
-        <img
-          width={"100%"}
-          src="https://www.pratikd.in/static/cb473f4754dd6b6b3790490ff4458800/2c936/banner.webp"
-        ></img>
+        <img width={"100%"} src="https://www.pratikd.in/static/cb473f4754dd6b6b3790490ff4458800/2c936/banner.webp"/> 
       </div>
     </div>
   );
