27213195860fe7e13da939f514179aa18758a068
summekoushal <2020bcs003@sggs.ac.in>
2026-07-11 09:30:15 +0530

    changes made temp to uncommit non final components

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ function App() {
     <>
       <Navbar />
       <Profile />
-      {/* <AboutMe /> */}
-      {/* <Projects /> */}
-      {/* <Contact /> */}
-      {/* <Footer /> */}
+      <AboutMe />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
